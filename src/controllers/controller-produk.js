fa9d9ed342cb391aa9b0e13cf4009a8eb3ea4a8f
DaffaFauzan <99790338+muhammaddaffafauzan@users.noreply.github.com>
2023-02-24 13:53:34 +0700

    Update controller-produk.js

--- src/controllers/controller-produk.js
+++ src/controllers/controller-produk.js
@@ -52,8 +52,64 @@ const getDataProduk = async (req, res) => {
             });
         } 
     }
+// mengubah data
+    const editDataProduk = async(req, res) => {
+        let id = req.params.id;
+        let dataEdit = {
+            nama: req.body.nama,
+            harga: req.body.harga,
+            stok: req.body.stok
+        }
+        const result = await new Promise((resolve, reject) => {
+             connection.query('UPDATE produk SET ? WHERE id = ?;', [dataEdit,id], function(error, rows) {
+                if (rows) {
+                    resolve(true);
+                } else {
+                    reject(false);
+                }
+             } );
+        });
+        if (result) {
+            res.send({
+                success: true,
+                message: 'Berhasil mengedit data',
+            });
+        } else{ 
+            res.send({
+                success: false,
+                message: 'Gagal mengedit data',
+        });
+    }
+}
 
+// menghapus data
+const deleteDataProduk = async(req, res) => {
+    let id = req.params.id;
+    
+    const result = await new Promise((resolve, reject) => {
+         connection.query('DELETE FROM produk WHERE id = ?;', [id], function(error, rows) {
+            if (rows) {
+                resolve(true);
+            } else {
+                reject(false);
+            };
+         } );
+    });
+    if (result) {
+        res.send({
+            success: true,
+            message: 'Berhasil menghapus data',
+        });
+    } else{ 
+        res.send({
+            success: false,
+            message: 'Gagal menghapus data',
+    });
+}
+}
     module.exports = {
         getDataProduk,
-        addDataProduk
+        addDataProduk,
+        editDataProduk,
+        deleteDataProduk
     }
