a2a7f15024cbbe1dd29e0ae8a7aed166f8fbe373
DaffaFauzan <99790338+muhammaddaffafauzan@users.noreply.github.com>
2023-02-25 07:48:45 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const routerProduk = require('./produk');
+const routerBiodata = require('./biodata');
 
 // GET localhost:8000/produk => Ambil data semua produk
 router.use('/produk', routerProduk);
+router.use('/biodata', routerBiodata);
 
 module.exports = router; 
